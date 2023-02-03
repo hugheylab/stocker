f94b67040dddcae7116bd2d0e49c01b96da8be74
Jake Hughey <jakejhughey@gmail.com>
2023-02-03 08:37:10 -0600

    don't rm.

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -3,7 +3,7 @@ set -e
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-rm Miniconda3-latest-Linux-x86_64.sh
+# rm Miniconda3-latest-Linux-x86_64.sh
 ~/miniconda3/bin/conda init bash
 
 source ~/miniconda3/etc/profile.d/conda.sh
