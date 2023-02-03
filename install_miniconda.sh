c0cb147f6ba09054d1911ac725a9632dbe97a820
Jake Hughey <jakejhughey@gmail.com>
2023-02-03 08:43:02 -0600

    restoring.

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -3,7 +3,7 @@ set -e
 
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh Miniconda3-latest-Linux-x86_64.sh -b -p ~/miniconda3
-# rm Miniconda3-latest-Linux-x86_64.sh
+rm Miniconda3-latest-Linux-x86_64.sh
 ~/miniconda3/bin/conda init bash
 
 source ~/miniconda3/etc/profile.d/conda.sh
@@ -15,3 +15,5 @@ conda config --add channels bioconda
 conda config --add channels conda-forge
 
 conda install mamba -c conda-forge
+
+echo "set -g mouse on" >> ~/.tmux.conf
